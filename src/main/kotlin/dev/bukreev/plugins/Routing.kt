c6b0e946492609276ed85c7ac4ed8859da79cd63
Evgeny Bukreev <ebukreev@ptsecurity.com>
2022-11-26 14:49:07 +0300

    #1 Create post request to upload file

--- src/main/kotlin/dev/bukreev/plugins/Routing.kt
+++ src/main/kotlin/dev/bukreev/plugins/Routing.kt
@@ -1,15 +1,31 @@
 package dev.bukreev.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.util.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post("/api/convert") {
+            call.receiveMultipart().forEachPart { part ->
+                if (part is PartData.FileItem) {
+                    val fileName = part.originalFileName as String
+
+                    val fileBytes = part.streamProvider().readBytes()
+
+                    withContext(Dispatchers.IO) {
+                        val file = File.createTempFile(fileName, UUID.randomUUID().toString())
+                        file.writeBytes(fileBytes)
+                    }
+                }
+
+                part.dispose()
+            }
         }
     }
 }
